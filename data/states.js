a98559c52599ef381d48a57c4ab5ca2b1a693e0d
Alan Urban <alan.urban23@gmail.com>
2022-07-09 16:26:31 +0200

    Refresh data

--- data/states.js
+++ data/states.js
@@ -2,14 +2,12 @@ const pomorskie = require('./pomorskie.json');
 const kujawskoPomorskie = require('./kujawskoPomorskie.json');
 const zachodnioPomorskie = require('./zachodnioPomorskie.json');
 const warminskoMazurskie = require('./warminskoMazurskie.json');
-const wielkopolskie = require('./wielkopolskie.json');
 
 export const states = [
     { name: 'pomorskie', ep: pomorskie},
     { name: 'kujawskoPomorskie', ep: kujawskoPomorskie},
     { name: 'zachodnioPomorskie', ep: zachodnioPomorskie},
     { name: 'warminskoMazurskie', ep: warminskoMazurskie},
-    { name: 'wielkopolskie', ep: wielkopolskie}
 ]
 
 export default states;
