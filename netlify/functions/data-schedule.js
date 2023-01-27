8c34287cac8efc264f961ba09ce34158616dfe9f
alan.urban <alan.urban@contractors.roche.com>
2023-01-27 01:10:43 +0100

    Netlify edge

--- netlify/functions/data-schedule.js
+++ netlify/functions/data-schedule.js
@@ -2,8 +2,7 @@ import fetch from 'node-fetch'
 import { schedule } from '@netlify/functions'
 
 // This is a sample build hook URL
-const BUILD_HOOK =
-  'https://api.netlify.com/build_hooks/63d30c033d3d2a10ff43157a'
+const BUILD_HOOK = 'https://api.netlify.com/build_hooks/63d30c033d3d2a10ff43157a';
 
 // Schedules the handler function to run at midnight on
 // Mondays, Wednesday, and Friday
